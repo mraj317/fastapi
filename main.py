70480c24d17a7463c9c456a19a873b47abb4017b
Maruti Raj <mraj317@gmail.com>
2023-07-13 16:37:06 +0530

    get posts api

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+my_posts = [
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "id": 1
+    },
+    {
+        "title": "title of post 2",
+        "content": "content of post 2",
+        "id": 2
+    }
+]
+
 
 @app.get("/")
 def root():
     return {"message": "World"}
+
+
+@app.get('/posts')
+async def get_posts():
+    return {"posts": my_posts}
