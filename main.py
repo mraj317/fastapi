7c80ff04f9e3910112fd9dc54b7fff6f1d2eff96
Maruti Raj <mraj317@gmail.com>
2023-07-13 18:26:26 +0530

    get on post, delete post api

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 my_posts = [
     {
@@ -16,16 +26,54 @@ my_posts = [
 ]
 
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
+
 @app.get("/")
 def root():
     return {"message": "World"}
+
+# ALL POSTS
 
 
 @app.get('/posts')
 async def get_posts():
     return {"posts": my_posts}
 
+# CREATE POST
+
+
+@app.post('/posts')
+async def create_post(post: Post):
+   # post["id"] = my_posts[-1]["id"] + 1
+    print(post.dict())
+    my_posts.append(post)
+    return {'post': post}
+
+# GET ONE POST
+
 
 @app.get('/posts/{post_id}')
 async def get_post(post_id: int):
-    return {"post_id": post_id}
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+    return {"post": post}
+
+# DELETE POST
+
+
+@app.delete('/posts/{post_id}')
+async def delete_post(post_id: int):
+    post = find_post(post_id)
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+
+    my_posts.remove(post)
+    return {'post': post}
